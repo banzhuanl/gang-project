6cbd24f2132913e5a722d1cf04d784f443163c18
banzhuanl <657356100@qq.com>
2023-08-05 10:10:49 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-header>
+      <img src="./assets/logo1.png" alt="">
+      <div class="head-title">个人中心</div>
+      <el-menu :default-active="activeIndex2" class="menu-header-right" mode="horizontal" @select="handleSelect"
+        background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="2"><router-link to="/user/login">个人中心</router-link></el-menu-item>
+        <el-menu-item index="3">充值中心</el-menu-item>
+        <el-menu-item index="3">客服中心</el-menu-item>
+        <el-menu-item index="3">新游开服表</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <div style="width: 1090px; margin: 0 auto;">
+        <router-view></router-view>
+      </div>
+    </el-main>
+    <el-footer></el-footer>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      activeIndex2: '2'
+    }
+  },
+  methods: {
+    handleSelect() {
+
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+.el-header {
+  background-color: #545c64;
+  color: #fff;
+}
+.head-title {
+  display: inline-block;
+  height: 60px;
+  line-height: 60px;
+  vertical-align: top;
+  margin-left: 30px;
+}
+.menu-header-right {
+  float: right;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
